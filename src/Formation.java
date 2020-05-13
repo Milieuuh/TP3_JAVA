d271303c96c9b3a10a6f7d012f18edc6e6692f85
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-05-13 08:24:14 +0200

    vdvcd

--- src/Formation.java
+++ src/Formation.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Formation {
+
+    //ATTRIBUTS
+    private ArrayList<Cours> listeCours;
+    private String codeFormation;
+    private String nomFormation;
+
+    //CONSTRUCTEUR
+    public Formation(String code, String nom)
+    {
+        this.listeCours = new ArrayList<>();
+        this.codeFormation = code;
+        this.nomFormation = nom;
+    }
+
+    //AUTRES METHODES
+    public String toString()
+    {
+        String s="";
+        for(int i=0; i<this.listeCours.size(); i++)
+        {
+            s+=listeCours.get(i).toString();
+        }
+        return s; 
+    }
+
 }
