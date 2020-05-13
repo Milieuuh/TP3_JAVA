aa484af8e8003814ec58499c119343843e3df0e9
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-05-13 08:22:34 +0200

    d

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //création des 7 cours
+        Cours c1 = new Cours("G201","Système",350);
+        Cours c2 = new Cours("G501","Hsitoire",50);
+        Cours c3 = new Cours("G502","Géographie",20);
+        Cours c4 = new Cours("G200","Mathématiques",450);
+        Cours c5 = new Cours("G101","Sport",10);
+        Cours c6 = new Cours("F101","Français",300);
+        Cours c7 = new Cours("F201","Anglais",5);
+
     }
 }
