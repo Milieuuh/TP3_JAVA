78479097fb79998bedd56cfc0a63d07d1057dca2
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-05-27 13:38:36 +0200

    r

---
+++ tp4ttrs/src/ErreurNotes.java
@@ -0,0 +1,6 @@
+public class ErreurNotes extends Exception {
+    public ErreurNotes(){
+        System.out.println("Vous avez sollicité un gestionnaire d’exceptions ? Je ne suis pas tout à fait prêt, mais je répond quand même");
+    }
+
+}
