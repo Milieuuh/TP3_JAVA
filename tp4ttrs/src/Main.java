40ecaad451eb0ccc440034f59202484ca4356a65
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-05-27 13:56:34 +0200

    Update Main.java

--- tp4ttrs/src/Main.java
+++ tp4ttrs/src/Main.java
@@ -2,7 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         Etudiant etu = new Etudiant("X", "Y", 28);
-        etu.ajouterNote(10);
-        etu.ajouterNote(15);
+        try {
+            etu.ajouterNote(12);
+            etu.ajouterNote(9);
+            etu.ajouterNote(7);
+            etu.ajouterNote(19);
+
+        } catch (ErreurNotes e)
+        {
+            System.out.println("Gestionnaire d’exceptions demandé...");
+        }
     }
 }
