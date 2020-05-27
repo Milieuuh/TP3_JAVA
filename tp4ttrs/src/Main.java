1ea142cf40786b0de23810472efb8ed66e5fd03a
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-05-27 13:38:49 +0200

    Update Main.java

--- tp4ttrs/src/Main.java
+++ tp4ttrs/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Etudiant etu = new Etudiant("X", "Y", 28);
+
+        etu.ajouterNote(10);
+        etu.ajouterNote(15);
     }
 }
